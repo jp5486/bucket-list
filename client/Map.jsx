b191cec7980b581deb546fee1fc6dd85ff872368
JoeMarion <joeyzmarion@gmail.com>
2016-01-12 17:50:27 -0800

    Add map functionality that is compatable with React.

--- client/Map.jsx
+++ client/Map.jsx
@@ -1,4 +1,64 @@
-Map = React.createClass({  
+var geocoder;
+var map;
+var currentLocation;
+
+Map = React.createClass({
+
+    initialize: function(location) {
+
+        currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+
+        var mapOptions = {
+          center: currentLocation,
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById("map-gic"),
+            mapOptions);
+
+        var infoWindowContent =   '<div class="info_content">' +
+                                  '<h3>Title Here</h3>' +
+                                  '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente totam exercitationem molestiae possimus.</p>' +
+                                  '</div>';
+
+        var infoWindow = new google.maps.InfoWindow({
+          content: infoWindowContent
+        });
+
+        geocoder = new google.maps.Geocoder();
+
+        var marker = new google.maps.Marker({
+          position: currentLocation,
+          map: map,
+          icon: 'http://i.stack.imgur.com/Kbx0I.png'
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infoWindow.open(map, marker);
+        });
+    },
+    codeAddress: function() {
+        var address = document.getElementById("address").value;
+        geocoder.geocode( {'address': address}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert("Geocode was not successful for the following reason: " + status);
+          }
+        });
+    },
+    getLocation: function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.initialize);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    },
     getDefaultProps: function () {
         return {
             initialZoom: 8,
@@ -7,11 +67,7 @@ Map = React.createClass({
         };
     },
     componentDidMount: function (rootNode) {
-        var mapOptions = {
-            center: this.mapCenterLatLng(),
-            zoom: this.props.initialZoom
-        },
-        map = new google.maps.Map(this.getDOMNode(), mapOptions);
+        this.getLocation();
         var marker = new google.maps.Marker({position: this.mapCenterLatLng(), title: 'Hi', map: map});
         this.setState({map: map});
     },
@@ -21,7 +77,9 @@ Map = React.createClass({
     },
     render: function () {
         return (
-        	<div className='map-gic'></div>
+            <div id='map-gic'></div>
+
+
         );
     }
 });
