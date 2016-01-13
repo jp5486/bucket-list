d716ac29a68ac9eb6341cc90f4824299d5458b64
Kai Huang <kaihuang@Kais-MacBook-Air.local>
2016-01-13 14:28:03 -0800

    Add content to about and credits pages

--- client/credits.jsx
+++ client/credits.jsx
@@ -1,15 +1,24 @@
-// Credits = React.createClass({
+Credits = React.createClass({
 
-// 	hidePage () {
-// 		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad1"))
-// 		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad2"))
-// 		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad3"))
-// 		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad4"))
-// 	},
+	hidePage () {
+		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad1"))
+		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad2"))
+		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad3"))
+		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad4"))
+	},
 	
-// 	render () {
-// 		return (
-
-// 	  ) 
-// 	}
-// })
+	render () {
+		return (
+			<div>
+				<ul>
+					<li><a href="https://www.meteor.com">Meteor</a></li>
+					<li><a href="https://www.mongodb.com">MongoDB</a></li>
+					<li><a href="facebook.github.io/react/">React</a></li>
+					<li><a href="cordova.apache.org/">Cordova</a></li>
+					<li><a href="meteor.com/blaze">Blaze</a></li>
+					<li><a href="https://github.com">Github</a></li>
+				</ul>
+			</div>
+	  ) 
+	}
+})
