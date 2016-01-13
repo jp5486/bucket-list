8d5e5a45198802ff2dc38635032ddf2915e2d2d3
Jeremy <jp5486@gmail.com>
2016-01-12 23:49:40 -0800

    protype navbar responsive design

--- client/Navbar.jsx
+++ client/Navbar.jsx
@@ -31,17 +31,43 @@ Navbar = React.createClass({
 		this.hidePage();
 	},
 
+	// render (){
+	// 	return (
+	// 		<nav className="navbar">
+	// 			<ul className="nav nav-tabs">
+	// 				<li role="presentation"><a href="#" onClick={this.renderHome}>Home</a></li>
+	// 				<li role="presentation"><a href="#" onClick={this.renderBucketList}>Bucket List</a></li>
+	// 				<li role="presentation"><a href="#" onClick={this.renderEvents}>Events</a></li>
+	// 				<li role="presentation"><a href="#" onClick={this.renderFriends}>Friends</a></li>
+	// 			</ul>
+	// 		</nav>
+	// 	)
+	// }
 	render (){
 		return (
-			<nav className="navbar">
-				<ul className="nav nav-tabs">
-					<li role="presentation"><a href="#" onClick={this.renderHome}>Home</a></li>
-					<li role="presentation"><a href="#" onClick={this.renderBucketList}>Bucket List</a></li>
-					<li role="presentation"><a href="#" onClick={this.renderEvents}>Events</a></li>
-					<li role="presentation"><a href="#" onClick={this.renderFriends}>Friends</a></li>
-				</ul>
+			<nav className="navbar navbar-inverse">
+			  <div className="container-fluid">
+			    <div className="navbar-header">
+			      <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			        <span className="icon-bar"></span>
+			        <span className="icon-bar"></span>
+			        <span className="icon-bar"></span>
+			      </button>
+			      <a className="navbar-brand glyphicon glyphicon-fire" href="#"></a>
+			    </div>
+			    <div className="collapse navbar-collapse" id="myNavbar">
+			      <ul className="nav navbar-nav">
+							<li className= "Active" role="presentation"><a href="#" onClick={this.renderHome}>Home</a></li>
+							<li role="presentation"><a href="#" onClick={this.renderBucketList}>Personal Bucket List</a></li>
+							<li role="presentation"><a href="#" onClick={this.renderEvents}>Events</a></li>
+							<li role="presentation"><a href="#" onClick={this.renderFriends}>Friends</a></li>
+			      </ul>
+			      <div className="nav navbar-nav navbar-right">
+			        <div id="render-login"></div>
+			      </div>
+			    </div>
+			  </div>
 			</nav>
 		)
 	}
 });
-
