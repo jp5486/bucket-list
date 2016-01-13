c26c5d1c5a5f91a03bc6b18b48d29e848f5873b1
Kai Huang <kaihuang@Kais-MacBook-Air.local>
2016-01-13 12:34:54 -0800

    Comment out code

--- client/about.jsx
+++ client/about.jsx
@@ -1,15 +1,15 @@
-About = React.createClass({
+// About = React.createClass({
 
-	hidePage () {
-		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad1"))
-		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad2"))
-		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad3"))
-		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad4"))
-	},
+// 	hidePage () {
+// 		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad1"))
+// 		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad2"))
+// 		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad3"))
+// 		ReactDOM.unmountComponentAtNode(document.getElementById("render-quad4"))
+// 	},
 
-	render () {
-		return (
+// 	render () {
+// 		return (
 
-	  ) 
-	}
-})
+// 	  ) 
+// 	}
+// })
