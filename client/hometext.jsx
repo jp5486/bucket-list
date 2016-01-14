13ef3866901d6826cb23823df3b12a22a7e0084b
Kai Huang <kaihuang@Kais-MacBook-Air.local>
2016-01-14 12:36:21 -0800

    Modify text styling

--- client/hometext.jsx
+++ client/hometext.jsx
@@ -1,7 +1,7 @@
 HomeText = React.createClass({
 	render() {
 		return (
-			<div className="text-container">
+			<div className="text-container landing" >
 				<p> Nobody should die with regrets. Here at MyBucketList.pw, we provide an application that will help individuals remind themselves of the goals they have set for themselves. </p>
 				<p> Whether you would like to spend your life making beautiful and meaningful things, or make sure that your life itself is beautiful and meaningful, our application will certainly be a wonderful partner throughout your life. </p>
 			</div>
