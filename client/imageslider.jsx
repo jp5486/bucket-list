8f264bb46caea4b856d353db0091d6588e2fddc0
Kai Huang <kaihuang@kais-air.attlocal.net>
2016-02-09 06:55:27 -0800

    Modify slider images

--- client/imageslider.jsx
+++ client/imageslider.jsx
@@ -68,7 +68,7 @@ StaticSlider = React.createClass({
     <input type="radio" name="radio-btn" id="img-1" defaultChecked />
     <li className="slide-container">
 		<div className="slide">
-			<img src="https://images.unsplash.com/photo-1452723312111-3a7d0db0e024?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=d2caa73026ef83d5701e1d6d3f1957a6"/>
+			<img src="https://images.unsplash.com/photo-1450149632596-3ef25a62011a?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=8955201cf904a73e41fd71d17af064c2"/>
         </div>
 		<div className="nav">
 			<label htmlFor="img-6" className="prev">&#x2039;</label>
@@ -90,7 +90,7 @@ StaticSlider = React.createClass({
     <input type="radio" name="radio-btn" id="img-3" />
     <li className="slide-container">
         <div className="slide">
-        	<img src="https://images.unsplash.com/photo-1418807817813-5847a371f1dd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=677b72c8297a74d05b773e483dbd8f85"/>
+        	<img src="https://images.unsplash.com/photo-1454991727061-be514eae86f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=7492bfdac7bee7d5b8e5a957625fe13f"/>
         </div>
 		<div className="nav">
 			<label htmlFor="img-2" className="prev">&#x2039;</label>
@@ -101,7 +101,7 @@ StaticSlider = React.createClass({
     <input type="radio" name="radio-btn" id="img-4" />
     <li className="slide-container">
         <div className="slide">
-        	<img src="https://images.unsplash.com/photo-1436891620584-47fd0e565afb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=9580e27795d8af74b7ea143e70a71636"/>
+        	<img src="https://images.unsplash.com/photo-1454942901704-3c44c11b2ad1?crop=entropy&dpr=0.67&fit=crop&fm=jpg&h=1075&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=2150"/>
         </div>
 		<div className="nav">
 			<label htmlFor="img-3" className="prev">&#x2039;</label>
@@ -123,7 +123,7 @@ StaticSlider = React.createClass({
     <input type="radio" name="radio-btn" id="img-6" />
     <li className="slide-container">
         <div className="slide">
-					<img src="https://images.unsplash.com/photo-1428434828181-9d110c490087?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=74f120a985f4e5a45dea5966f5dce182"/>
+					<img src="https://images.unsplash.com/photo-1443453489887-98f56bc5bb38?crop=entropy&dpr=0.67&fit=crop&fm=jpg&h=1075&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=2150"/>
         </div>
 		<div className="nav">
 			<label htmlFor="img-5" className="prev">&#x2039;</label>
